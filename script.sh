44f8e08435fbdafa6fdc3683c1d982c277e868ea
bluejet518 <40314565+bluejet518@users.noreply.github.com>
2018-07-02 12:39:59 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 apt-get update
 yes | apt-get install squid3 apache2-utils
 wget -O /etc/squid3/squid.conf https://raw.githubusercontent.com/bluejet518/internet/master/squid.conf --no-check-certificate
-htpasswd -c -b /etc/squid3/passwd gcloud proxy123
+htpasswd -c -b /etc/squid3/passwd bigspoon proxy123
 /etc/init.d/squid3 restart
