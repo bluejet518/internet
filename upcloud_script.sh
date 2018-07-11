88b4b8fe891ff72d8cd55b7a64b19fa59c6c988f
bluejet518 <40314565+bluejet518@users.noreply.github.com>
2018-07-11 15:25:46 -0500

    Update upcloud_script.sh

--- upcloud_script.sh
+++ upcloud_script.sh
@@ -1,5 +1,5 @@
 apt-get update
 yes | apt-get install squid3 apache2-utils
-wget -O /etc/squid3/squid.conf https://raw.githubusercontent.com/bluejet518/internet/master/squid.conf --no-check-certificate
+wget -O /etc/squid3/squid.conf https://raw.githubusercontent.com/bluejet518/internet/master/upcloudsquid.conf --no-check-certificate
 htpasswd -c -b /etc/squid3/passwd upcloud proxy123
 /etc/init.d/squid3 restart
